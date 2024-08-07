d0e1c9d2210410b694703c1a9c6d0b7f8b518e10
Nelson Geovanny Ortiz Morales <nortiz@mobiletec.net>
2024-08-07 16:55:57 -0500

    Initial commit: add GitClass.cs

---
+++ GitClasscs.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OOP_Project
+{
+    class GitClasscs
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Git");
+        }
+    }//
+}
